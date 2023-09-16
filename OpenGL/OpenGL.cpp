797140dbc095d5fb2d5fc813aa6a8bacaa397adf
strurao <strurao@gmail.com>
2023-09-16 23:06:40 +0900

    초기 커밋.

---
+++ OpenGL/OpenGL.cpp
@@ -0,0 +1,18 @@
+﻿#include <GL/glut.h>
+
+void draw(void) {
+    glColor3f(1.0, 1.0, 1.0);
+    glutWireTeapot(0.5);
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(300, 300);
+    glutCreateWindow("strurao");
+    glutDisplayFunc(draw);
+    glutMainLoop();
+    return 0;
+}
---
+++ OpenGL/main.c
@@ -0,0 +1,19 @@
+
+/*#include <GL/glut.h>
+
+void draw(void) {
+    glColor3f(1.0, 1.0, 1.0);
+    glutWireTeapot(0.5);
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(300, 300);
+    glutCreateWindow("12000000 인트아이");
+    glutDisplayFunc(draw);
+    glutMainLoop();
+    return 0;
+}*/
